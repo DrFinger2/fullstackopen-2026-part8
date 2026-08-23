5a593e5164d8521112eb76c163ab7ab128640417
DoctorFinger <144325868+DoctorFinger@users.noreply.github.com>
2026-08-23 06:58:30 +0300

    maybe buggy code

--- library-frontend/src/main.jsx
+++ library-frontend/src/main.jsx
@@ -5,10 +5,22 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { HttpLink } from "@apollo/client";
 import App from "./App.jsx";
 import AuthProvider from "./contex/AuthProvider.jsx";
+
+const authLink = new SetContextLink((prevContext) => {
+  const token = localStorage.getItem("library-user-token");
+  return {
+    headers: {
+      ...prevContext.headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/",
+});
+
 const client = new ApolloClient({
-  link: new HttpLink({
-    uri: "http://localhost:4000/",
-  }),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
